df14a8f536aa5f8a7355b2c453a551ff17fa412c
Sunitha Rani <46861022+suni10@users.noreply.github.com>
2020-08-20 10:18:47 +0530

    Update views.py

--- mini/dataED/views.py
+++ mini/dataED/views.py
@@ -20,57 +20,49 @@ def resencrypt(request):
     message=request.POST['string']
     key=int(request.POST['key'])
     new_msg=''
-    k=key
-    while(k>0):
-       new_msg=''
-       for i in message:
-            if(i.isalpha()):
-                if(i.isupper()):
-                    position=alpha_up.index(i)
-                    newpos=(position+key)%26
-                    new_msg+=alpha_up[newpos]
-                elif(i.islower()):
-                    position=alphabet.index(i)
-                    newpos=(position+key)%26
-                    new_msg+=alphabet[newpos]
-            elif(i.isdigit()):
-                position=nums.index(i)
-                newpos=(position+key)%10
-                new_msg+=nums[newpos]
-            else:
-                position=sp_char.index(i)
-                newpos=(position+key)%29
-                new_msg+=sp_char[newpos]
-       k-=1 
-       message=new_msg
+    for i in message:
+        if(i.isalpha()):
+            if(i.isupper()):
+                position=alpha_up.index(i)
+                newpos=(position+key)%26
+                new_msg+=alpha_up[newpos]
+            elif(i.islower()):
+                position=alphabet.index(i)
+                newpos=(position+key)%26
+                new_msg+=alphabet[newpos]
+        elif(i.isdigit()):
+            position=nums.index(i)
+            newpos=(position+key)%10
+            new_msg+=nums[newpos]
+        else:
+            position=sp_char.index(i)
+            newpos=(position+key)%29
+            new_msg+=sp_char[newpos]
+    message=new_msg
     return render(request,'resenc.html',{"new_msg":message})
 def resdecrypt(request):
     message=request.POST['string']
     key=int(request.POST['key'])
     original_msg=''
-    k=key
-    while(k>0):
-        original_msg=''
-        for i in message:
-            if(i.isalpha()):
-                if(i.isupper()):
-                    position=alpha_up.index(i)
-                    newpos=(position-key)%26
-                    original_msg+=alpha_up[newpos]
-                elif(i.islower()):
-                    position=alphabet.index(i)
-                    newpos=(position-key)%26
-                    original_msg+=alphabet[newpos]
-            elif(i.isdigit()):
-                position=nums.index(i)
-                newpos=(position-key)%10
-                original_msg+=nums[newpos]
-            else:
-                position=sp_char.index(i)
-                newpos=(position-key)%29
-                original_msg+=sp_char[newpos]
-        k-=1
-        message=original_msg
+    for i in message:
+        if(i.isalpha()):
+            if(i.isupper()):
+                position=alpha_up.index(i)
+                newpos=(position-key)%26
+                original_msg+=alpha_up[newpos]
+            elif(i.islower()):
+                position=alphabet.index(i)
+                newpos=(position-key)%26
+                original_msg+=alphabet[newpos]
+        elif(i.isdigit()):
+            position=nums.index(i)
+            newpos=(position-key)%10
+            original_msg+=nums[newpos]
+        else:
+            position=sp_char.index(i)
+            newpos=(position-key)%29
+            original_msg+=sp_char[newpos]
+    message=original_msg
     return render(request,'resdec.html',{"original_msg":message})
 def home1(request):
     return render(request,'home.html')
